f91607910be4769741da921d60ac8a39becf9977
Lilian <lilianvarrel@hotmail.fr>
2021-11-17 16:48:03 +0100

    SDK First commit

---
+++ run.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+export COMPOSE_FILE_PATH="${PWD}/target/classes/docker/docker-compose.yml"
+
+if [ -z "${M2_HOME}" ]; then
+  export MVN_EXEC="mvn"
+else
+  export MVN_EXEC="${M2_HOME}/bin/mvn"
+fi
+
+start() {
+    docker volume create edf-demo-dev-alfresco-acs-volume
+    docker volume create edf-demo-dev-alfresco-db-volume
+    docker volume create edf-demo-dev-alfresco-ass-volume
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d
+}
+
+start_share() {
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d edf-demo-dev-alfresco-share
+}
+
+start_acs() {
+    docker-compose -f "$COMPOSE_FILE_PATH" up --build -d edf-demo-dev-alfresco-acs
+}
+
+down() {
+    if [ -f "$COMPOSE_FILE_PATH" ]; then
+        docker-compose -f "$COMPOSE_FILE_PATH" down
+    fi
+}
+
+purge() {
+    docker volume rm -f edf-demo-dev-alfresco-acs-volume
+    docker volume rm -f edf-demo-dev-alfresco-db-volume
+    docker volume rm -f edf-demo-dev-alfresco-ass-volume
+}
+
+build() {
+    $MVN_EXEC clean package
+}
+
+build_share() {
+    docker-compose -f "$COMPOSE_FILE_PATH" kill edf-demo-dev-alfresco-share
+    yes | docker-compose -f "$COMPOSE_FILE_PATH" rm -f edf-demo-dev-alfresco-share
+    $MVN_EXEC clean package -pl edf-demo-dev-alfresco-share,edf-demo-dev-alfresco-share-docker
+}
+
+build_acs() {
+    docker-compose -f "$COMPOSE_FILE_PATH" kill edf-demo-dev-alfresco-acs
+    yes | docker-compose -f "$COMPOSE_FILE_PATH" rm -f edf-demo-dev-alfresco-acs
+    $MVN_EXEC clean package -pl edf-demo-dev-alfresco-integration-tests,edf-demo-dev-alfresco-platform,edf-demo-dev-alfresco-platform-docker
+}
+
+tail() {
+    docker-compose -f "$COMPOSE_FILE_PATH" logs -f
+}
+
+tail_all() {
+    docker-compose -f "$COMPOSE_FILE_PATH" logs --tail="all"
+}
+
+prepare_test() {
+    $MVN_EXEC verify -DskipTests=true -pl edf-demo-dev-alfresco-platform,edf-demo-dev-alfresco-integration-tests,edf-demo-dev-alfresco-platform-docker
+}
+
+test() {
+    $MVN_EXEC verify -pl edf-demo-dev-alfresco-platform,edf-demo-dev-alfresco-integration-tests
+}
+
+case "$1" in
+  build_start)
+    down
+    build
+    start
+    tail
+    ;;
+  build_start_it_supported)
+    down
+    build
+    prepare_test
+    start
+    tail
+    ;;
+  start)
+    start
+    tail
+    ;;
+  stop)
+    down
+    ;;
+  purge)
+    down
+    purge
+    ;;
+  tail)
+    tail
+    ;;
+  reload_share)
+    build_share
+    start_share
+    tail
+    ;;
+  reload_acs)
+    build_acs
+    start_acs
+    tail
+    ;;
+  build_test)
+    down
+    build
+    prepare_test
+    start
+    test
+    tail_all
+    down
+    ;;
+  test)
+    test
+    ;;
+  *)
+    echo "Usage: $0 {build_start|build_start_it_supported|start|stop|purge|tail|reload_share|reload_acs|build_test|test}"
+esac
